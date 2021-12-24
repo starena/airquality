7270a7ec6f3bf6192bc1f3b8622327fbd4a42b9b
Starena <stef@ziza.be>
2021-12-24 08:21:33 +0100

    Mise à jour pxt.json, main.ts, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,13 @@
-// les tests vont ici ; cela ne sera pas compilé si ce paquet est utilisé en tant qu'extension.
+let eCO2 = 0
+let TVOCs = 0
+basic.forever(() => {
+    TVOCs = airQuality.readTvoc()
+    eCO2 = airQuality.readCo2()
+    basic.showString("TVOC")
+    basic.showNumber(TVOCs)
+    basic.pause(1000)
+    basic.showString("eCO2")
+    basic.showNumber(eCO2)
+    basic.pause(1000)
+})
+airQuality.appStart()
